d027e85d060c97f95c6d2fae66d4c314833a205c
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2023-01-21 00:28:18 +0800

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -17,27 +17,27 @@ let ri = '25'
 let pk = '30'
 let info = `
 ╭━━━━「 *SEWA* 」
-┊⫹⫺ *Hemat:* _${sh}k/grup (7 Hari)_
-┊⫹⫺ *Normal:* _${sn}k/grup (1 bulan)_                                         
-┊⫹⫺ *permanen:* = _${sv}k/grup (unlimited)_
+┊⫹⫺ *Hemat:* _Rm ${sh}/grup (7 Hari)_
+┊⫹⫺ *Normal:* _Rm ${sn}/grup (1 bulan)_                                         
+┊⫹⫺ *permanen:* = _Rm ${sv}/grup (unlimited)_
 ╰═┅═━––––––๑
 
 ╭━━━━「 *PREMIUM* 」
-┊⫹⫺ *Hemat:* _${ph}k (7 Hari)_
-┊⫹⫺ *Normal:* _${pn}k (1 bulan)_                                       
-┊⫹⫺ *Permanent:* = _${ppm}k (Unlimited)_
+┊⫹⫺ *Hemat:* _Rm ${ph} (7 Hari)_
+┊⫹⫺ *Normal:* _Rm ${pn} (1 bulan)_                                       
+┊⫹⫺ *Permanent:* = _Rm ${ppm} (Unlimited)_
 ╰═┅═━––––––๑
 ╭━━━━「 *PERPANJANG SEWA* 」
-┊⫹⫺ *Hemat:* _3k (7 Hari)_
-┊⫹⫺ *Normal:* _6k (1 bulan)_                                       
+┊⫹⫺ *Hemat:* _Rm 3 (7 Hari)_
+┊⫹⫺ *Normal:* _Rm 6 (1 bulan)_                                       
 ╰═┅═━––––––๑
 ╭━━━━「 *PERPANJANG PREMIUM* 」
-┊⫹⫺ *Hemat:* _3k (7 Hari)_
-┊⫹⫺ *Normal:* _6k (1 bulan)_                                       
+┊⫹⫺ *Hemat:* _Rm 3 (7 Hari)_
+┊⫹⫺ *Normal:* _Rm 6 (1 bulan)_                                       
 ╰═┅═━––––––๑
 ╭━━━━「 *JASARUN* 」
-┊⫹⫺ *Hemat:* _${ri}k (7 Hari)_
-┊⫹⫺ *Normal:* _${pk}k (1 bulan)_                                       
+┊⫹⫺ *Hemat:* _Rm ${ri} (7 Hari)_
+┊⫹⫺ *Normal:* _Rm ${pk} (1 bulan)_                                       
 ╰═┅═━––––––๑
 ༅ _*FITUR BOT*
 
@@ -53,8 +53,8 @@ let info = `
 _Total Fitur:_ 570++
 
 *⫹⫺ PAYMENT:*
-• *Dana:*
-• *Gopay:*
+• *Digi:*
+• *Maxis:*
 • *Ovo:*
 • *QRIS*
 • *ALL PAYMENT*
@@ -71,22 +71,22 @@ const sections = [
    {
 	title: `${htjava} SEWA ✦-------`,
 	rows: [
-	    {title: "🔖 HEMAT", rowId: '.order *Paket:* 5K • Sewa', description: 'PRICE: ' + sh + 'k (7 Hari)' },
-	    {title: "🔖 NORMAL", rowId: '.order *Paket:* 10K • Sewa', description: 'PRICE: ' + sn + 'k (1 bulan)' },
-	{title: "🔖 Permanen", rowId: '.order *Paket:* 40K • Sewa', description: 'PRICE: ' + sv + 'k (Permanen)' },
+	    {title: "🔖 HEMAT", rowId: '.order *Paket:* Rm 5 • Sewa', description: 'PRICE: Rm' + sh + ' (7 Hari)' },
+	    {title: "🔖 NORMAL", rowId: '.order *Paket:* Rm 10 • Sewa', description: 'PRICE: Rm' + sn + ' (1 bulan)' },
+	{title: "🔖 Permanen", rowId: '.order *Paket:* Rm 40 • Sewa', description: 'PRICE: Rm' + sv + ' (Permanen)' },
 	]
     }, {
     title: `${htjava} PREMIUM ✦-------`,
 	rows: [
-	    {title: "🌟 HEMAT", rowId: '.order *Paket:* 5K • Premium', description: 'PRICE: ' + ph + 'k (7 Hari)' },
-	    {title: "🌟 NORMAL", rowId: '.order *Paket:* 10K • Premium', description: 'PRICE: ' + pn + 'k (1 bulan)' },
-	{title: "🌟 PERMANENT", rowId: '.order *Paket:* 35K • Premium', description: 'PRICE: ' + ppm + 'k (UNLIMITED)' },
+	    {title: "🌟 HEMAT", rowId: '.order *Paket:* Rm 5 • Premium', description: 'PRICE: Rm' + ph + ' (7 Hari)' },
+	    {title: "🌟 NORMAL", rowId: '.order *Paket:* Rm 10 • Premium', description: 'PRICE: Rm' + pn + ' (1 bulan)' },
+	{title: "🌟 PERMANENT", rowId: '.order *Paket:* Rm 35 • Premium', description: 'PRICE: Rm' + ppm + ' (UNLIMITED)' },
 	]
     }, {
         title: `${htjava} JASA RUN ✦-------`,
 	rows: [
-	    {title: "🌟 HEMAT", rowId: '.order *Paket:* 25K • Jasarun', description: 'PRICE: ' + ri + 'k (25 Hari)' },
-	    {title: "🌟 NORMAL", rowId: '.order *Paket:* 30K • Jasarun', description: 'PRICE: ' + pk + 'k (1 bulan)' },
+	    {title: "🌟 HEMAT", rowId: '.order *Paket:* Rm 25 • Jasarun', description: 'PRICE: Rm' + ri + ' (25 Hari)' },
+	    {title: "🌟 NORMAL", rowId: '.order *Paket:* Rm 30 • Jasarun', description: 'PRICE: Rm' + pk + ' (1 bulan)' },
 	]
     },
 ]
